fd32f715749ae6545d93538690301d03e333fcff
Carlos Zamora <czamora5@ucol.mx>
2021-08-25 18:23:02 -0500

    Implemented Runner class

--- carrera/main.js
+++ carrera/main.js
@@ -1,5 +1,26 @@
 class Dice {
-	roll() {
+	static roll() {
 		return Math.floor(Math.random()*6 + 1);
 	}
+}
+
+class Runner {
+    constructor() {
+        this._position = 0;
+    }
+
+    getPosition() {
+        return this._position;
+    }
+
+    move() {
+        let result = Dice.roll();
+        if (result >= 4) {
+            this._position += 2;
+        } else if (result >= 2) {
+            this._position += 1;
+        } else {
+            this._position += 3;
+        }
+    }
 }
