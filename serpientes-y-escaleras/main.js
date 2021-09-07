5d1a40cf530089ed0c98c386d73b6afe18e6e342
Carlos Zamora <czamora5@ucol.mx>
2021-09-07 13:18:42 -0500

    Included messages describing the player positions and dice results

--- serpientes-y-escaleras/main.js
+++ serpientes-y-escaleras/main.js
@@ -20,9 +20,12 @@ class Player {
 
 class Board {
     constructor() {
+        // Tenía pensado hacer un vector de jugadores, lo cual iba a facilitar el método
+        // para desplazar los jugadores, pero también iba a dificultar los mensajes en consola de
+        // así que decidí crear ambos jugadores como distintos atributos de la clase.
         this._playerA = new Player();
         this._playerB = new Player();
-        this._squares = new Array(101).fill(0);
+        this._squares = new Array(101).fill(0); // Empezamos con todas las casillas vacias
         this._buildBoard(); // Marcamos las serpientes y escaleras
     }
 
@@ -56,11 +59,36 @@ class Board {
         // Primero tiramos el dado y nos movemos el número de casillas que marque el dado,
         // luego nos movemos el número de casillas indicadas en la nueva posición del tablero,
         // en caso de que no caigamos en serpientes ni en escaleras nos moveremos cero casillas.
-        this._playerA.move(Dice.roll());
-        if (this._playerA.getPosition() < 100) this._playerA.move(this._squares[this._playerA.getPosition()]);
+        let result = Dice.roll();
+        this._playerA.move(result);
+        console.log(`El primer jugador avanza ${result} casillas`);
+        if (this._playerA.getPosition() < 100) { // Si el jugador no ha ganado entonces puede haber caído en una casilla especial
+            let nextMove = this._squares[this._playerA.getPosition()]
+            if (nextMove > 0) {
+                console.log(`¡El primer jugador cayó en una escalera! Avanza ${nextMove} casillas`);
+            } else if (nextMove < 0) {
+                console.log(`¡El primer jugador cayó en una serpiente! Retrocede ${-nextMove} casillas`);
+            }
+            this._playerA.move(nextMove);
+        } else { // En caso de que ya haya ganado simplemente salimos de la función ya que no es necesario que el siguiente jugador avance
+            console.log(`¡El primer jugador ganó!`);
+            return; 
+        }
         
-        this._playerB.move(Dice.roll());
-        if (this._playerB.getPosition() < 100) this._playerB.move(this._squares[this._playerB.getPosition()]);
+        result = Dice.roll();
+        console.log(`El segundo jugador avanza ${result} casillas`);
+        this._playerB.move(result);
+        if (this._playerB.getPosition() < 100) {
+            let nextMove = this._squares[this._playerB.getPosition()]
+            if (nextMove > 0) {
+                console.log(`¡El segundo jugador cayó en una escalera! Avanza ${nextMove} casillas`);
+            } else if (nextMove < 0) {
+                console.log(`¡El segundo jugador cayó en una serpiente! Retrocede ${-nextMove} casillas`);
+            }
+            this._playerB.move(nextMove);
+        } else {
+            console.log(`¡El segundo jugador ganó!`);
+        }
     }
 }
 
@@ -74,11 +102,3 @@ do {
 
     console.log(`Posición del primer jugador: ${positions[0]}, posición del segundo jugador: ${positions[1]}`);
 } while(positions[0] < totalSquares && positions[1] < totalSquares)
-
-if (positions[0] >= totalSquares && positions[1] >= totalSquares) {
-    console.log('Hubo un empate');
-} else if (positions[0] >= totalSquares) {
-    console.log('El primer jugador ganó');
-} else {
-    console.log('El segundo jugador ganó');
-}
