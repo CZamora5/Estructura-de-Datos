ed586f4b63882c6acb0444550b59d55ee6e0a839
Carlos Zamora <czamora5@ucol.mx>
2021-09-20 00:34:27 -0500

    Added a driver class in main

--- app-control-de-inventario/main.js
+++ app-control-de-inventario/main.js
@@ -1,2 +1,19 @@
 import Inventory from './inventory.js'
 import Product from './product.js'
+
+class App {
+    constructor() {
+        this._inventory = new Inventory();
+        
+        this._btnAdd = document.getElementById('btnAdd');
+        this._btnReset = document.getElementById('btnReset');
+        this._btnDelete = document.getElementById('btnDelete');
+        this._btnSearch = document.getElementById('btnSearch');
+        this._btnList = document.getElementById('btnList');
+        this._btnInverse = document.getElementById('btnInverse');
+        this._btnInsert = document.getElementById('btnInverse');
+    }
+}
+
+// Creamos una instancia para habilitar los event listeners
+new App();
