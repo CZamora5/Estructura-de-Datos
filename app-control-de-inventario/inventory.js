7fe603f1a26361f122c84803d7c2b72014ffbe3e
Carlos Zamora <czamora5@ucol.mx>
2021-09-20 00:17:48 -0500

    Implemented getProducts and insertAt methods in Inventory class

--- app-control-de-inventario/inventory.js
+++ app-control-de-inventario/inventory.js
@@ -9,10 +9,28 @@ export default class Inventory {
 			if (product.getId() == id) return product; 
 		}
 
-		return false;
+		return null;
 	}
 
 	getLength() {
 		return this._products.length;
+	}
+
+	getProducts() {
+		return this._products;
+	}
+
+	/* Methods */
+	insertAt(product, index) {
+		if (0 > index || index >= this._products.length) return false;
+
+		let previous = product; 
+		for (let i = index; i <= this._products.length; i--) {
+			let auxiliar = this._products[i];
+			this._products[i] = previous;
+			previous = auxiliar;
+		}
+		this._products.push(previous);
+		return true;
 	}
 }
