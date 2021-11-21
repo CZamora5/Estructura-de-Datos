854751a211a20dab365c4cb72a8c3991b3e2e323
Carlos Zamora <czamora5@ucol.mx>
2021-11-21 07:22:21 -0600

    Implemented circular linked list

--- listas-circulares/circularList.js
+++ listas-circulares/circularList.js
@@ -1,28 +1,79 @@
 export default class CircularList {
 	constructor() {
 		this._head = null;
-		this._length = 0;
-	}
-	
-	/* Getter Methods*/
-	get length() {
-		return this._length;
+		this._tail = null;
 	}
 
 	/* Public Methods */
-	getProductById(id) {
+	getStationByName(name) {
 		let tmp = this._head;
-		while (tmp !== null) {
-			if (tmp.id === id) 
-				return tmp;
+
+		if (tmp === null) return tmp;
+		if (this._tail.name === name) return this._tail;
+
+		while (tmp.next !== this._head) {
+			if (tmp.name === name) return tmp;
 			tmp = tmp.next;
 		}
 		return null;
 	}
 
+	addStation(station) {
+		if (alreadyExists(station)) return false;
+
+		if (this._head === null) {
+			this._head = station;
+			this._tail = station;
+			station.next = station;
+			station.previous = station;
+		} else {
+			this._head.previous = station;
+			this._tail.next = station;
+			station.next = this._head;
+			station.previous = this._tail;
+			this._tail = station;
+		}
+
+		return true;
+	}
+
+	removeByName(name) {
+		let tmp = this._head;
+
+		if (tmp === null) return tmp;
+
+		if (this._head.name === name && this._tail.name === name) {
+			this._head = null;
+			this._tail = null;
+			tmp.previous = null;
+			tmp.next = null;
+			return tmp;
+		}
+
+		if (this._head.name === name) {
+			this._head = tmp.next;
+		} else if (this._tail.name === name) {
+			tmp = this._tail;
+			this._tail = tmp.previous;
+		} else {
+			while (tmp.next !== this._head) {
+				if (tmp.name === name) break;
+				tmp = tmp.next;
+			}
+		}
+
+		if (tmp !== null) {
+			tmp.previous.next = tmp.next;
+			tmp.next.previous = tmp.previous;
+			tmp.next = null;
+			tmp.previous = null;
+		}
+		return tmp;
+	}
+
 	getList() {
 		if (this._head === null) {
-			return '<strong>No hay productos en inventario<strong>'
+			return '<strong>No hay bases en la ruta<strong>';
 		} 
 
 		return this._getList();
@@ -30,77 +81,20 @@ export default class CircularList {
 
 	getReverseList() {
 		if (this._head === null) {
-			return '<strong>No hay productos en inventario<strong>'
-		} 
+			return '<strong>No hay bases en la ruta<strong>';
+		}
 
 		return this._getReverseList();
 	}
 
-	addProduct(product) {
-		if (this.alreadyExists(product)) 
-			return false;
-			
+	alreadyExists(station) {
 		let tmp = this._head;
 
-		if (tmp === null) {
-			this._head = product;
-		} else if (tmp.id > product.id) {
-			this._head = product;
-			tmp.previous = product;
-			product.next = tmp
-		} else {
-			while (tmp.next !== null && tmp.next.id < product.id) {
-				tmp = tmp.next;
-			}
+		if (tmp === null) return false;
+		if (this._tail.name === station.name) return true;
 
-			if (tmp.next !== null) {
-				tmp.next.previous = product;
-				product.next = tmp.next;
-			}
-
-			tmp.next = product;
-			product.previous = tmp;
-		}
-
-		this._length++;
-		return true;
-	}
-
-	removeById(id) {
-		let product = null;
-		let tmp = this._head;
-
-		if (tmp === null) return product;
-		
-		if (tmp.id === id) {
-			product = tmp;
-			this._head = product.next;
-		} else {
-			while (tmp.next !== null) {
-				if (tmp.next.id === id) {
-					product = tmp.next;
-					tmp.next = product.next;
-					if (product.next !== null) 
-						product.next.previous = product.previous;
-					break;
-				}
-				tmp = tmp.next;
-			}
-		}
-
-		if (product === null) return product;
-
-		product.previous = null;
-		product.next = null;
-		this._length--;
-		return product;
-	}
-
-	alreadyExists(product) {
-		let tmp = this._head;
-		while (tmp !== null) {
-			if (tmp.id === product.id)
-				return true;
+		while (tmp.next !== this._head) {
+			if (tmp.name === station.name) return true;
 			tmp = tmp.next;
 		}
 		return false;
@@ -108,18 +102,18 @@ export default class CircularList {
 	
 	/* Private Methods */
 	_getList(node = this._head, nodeIndex = 1) {
-		if (node === null) return '';
+		if (node === this._head && nodeIndex > 1) return '';
 		let nodeInfo = `
-			<strong>Producto #${nodeIndex}</strong><br>
+			<strong>Base #${nodeIndex}</strong><br>
 			${node.getInfo()}<br>
 		`;
 		return nodeInfo + this._getList(node.next, nodeIndex + 1);
 	}
 
 	_getReverseList(node = this._head, nodeIndex = 1) {
-		if (node === null) return '';
+		if (node === this._head && nodeIndex > 1) return '';
 		let nodeInfo = `
-			<strong>Producto #${nodeIndex}</strong><br>
+			<strong>Base #${nodeIndex}</strong><br>
 			${node.getInfo()}<br>
 		`;
 		return this._getReverseList(node.next, nodeIndex + 1) + nodeInfo;
--- listas-circulares/station.js
+++ listas-circulares/station.js
@@ -1,42 +1,42 @@
 export default class Station {
-    constructor(name, duration) {
-        this._name = name;
-        this._duration = duration;
-        this._next = null;
-        this._prev = null;
-    }
+  constructor(name, duration) {
+    this._name = name;
+    this._duration = duration;
+    this._next = null;
+    this._prev = null;
+  }
 
-    /* Getter Methods */
-    get name() {
-        return this._name;
-    }
+  /* Getter Methods */
+  get name() {
+    return this._name;
+  }
 
-    get duration() {
-        return this._duration;
-    }
+  get duration() {
+    return this._duration;
+  }
 
-    get next() {
-        return this._next;
-    }
+  get next() {
+    return this._next;
+  }
 
-    get previous() {
-        return this._prev;
-    }
+  get previous() {
+    return this._prev;
+  }
 
-    /* Setter Methods */
-    set next(station) {
-        this._next = station;
-    }
+  /* Setter Methods */
+  set next(station) {
+    this._next = station;
+  }
 
-    set previous(station) {
-        this._prev = station;
-    }
+  set previous(station) {
+    this._prev = station;
+  }
 
-    /* Public Methods */
-    getInfo() {
+  /* Public Methods */
+  getInfo() {
     return `
-            Nombre de la base: ${this._name}<br>
-            Duración del recorrido: ${this._duration}<br>
-        `;
-    }
+      Nombre de la base: ${this._name}<br>
+      Duración del recorrido: ${this._duration}<br>
+    `;
+  }
 }
